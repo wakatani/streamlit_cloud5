37397d3afa7b63286f0f070dc8357c898a20ab79
Akiyoshi Wakatani <wakatani@konan-u.ac.jp>
2025-06-11 14:01:59 +0900

    June 11

--- GPTapp.py
+++ GPTapp.py
@@ -59,8 +59,26 @@ if lang=="Japanese":
 else:
   language="英語"
 
-model="gpt-4o-2024-08-06"
-  
+'''
+gpt-4.5-preview
+gpt-4o
+gpt-4o-mini
+o1
+o1-mini
+o3-mini
+o3
+'''
+#model="gpt-4o-2024-08-06"
+model = st.radio(label='model',
+                options=('gpt-4o', 'gpt-4o-mini', 'gpt-4.5-preview'
+                         'o1','o1-mini','o3','o3-mini'),
+                index=0,
+                horizontal=True,
+)
+
+
+
+
 #
 # 問題作成の元になる文章群
 #
